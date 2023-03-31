55ba29f8c5b2a80617774aabd4e205b020d007a2
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2023-03-30 20:00:35 -0700

    Add theoretical pattern figure generator * Comes with a correction found while working on the measured patterns * Corrected patterns file is attached

---
+++ uma8_theoretical_patterns/uma8_theoretical_patterns.py
@@ -0,0 +1,38 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+c = 343
+frequencies = [700, 1400, 2800, 5600]
+incoming_angles = np.linspace(0, 2*np.pi, 1000)
+X = np.array([ # estimated positions of the microphones
+    [0, 0],
+    [0, 0.045],
+    [0.039, 0.023],
+    [0.039, -0.023],
+    [0, -0.045],
+    [-0.039, -0.023],
+    [-0.039, 0.023],
+])
+target_dir = np.array([1, 0])
+
+def W(k, target_k):
+    weights = (1/len(X))*np.exp(1j*X@target_k)
+    # window_factor = 0.30 # based on the geometry, this is what all kinds of windows reduce to---a single factor
+    # weights = np.array([window_factor]+[(1-window_factor)/6]*6)*np.exp(1j*(1/c)*X@target_k)
+    return weights.dot(np.exp(-1j*X@k))
+
+incoming_w_over_freq = []
+for freq in frequencies:
+    target_k = (2*np.pi*freq/c)*target_dir
+    incoming_k = [np.array([np.cos(theta), np.sin(theta)])*(2*np.pi*freq)/c for theta in incoming_angles]
+    incoming_w = np.array([W(k, target_k) for k in incoming_k])
+    incoming_w_over_freq.append(incoming_w)
+
+plt.figure(1)
+for freq, incoming_w in zip(frequencies, incoming_w_over_freq):
+    plt.polar(incoming_angles, 20*np.log10(np.abs(incoming_w)), label=f'{freq} Hz')
+plt.ylim([-24, 0])
+plt.legend()
+plt.title('UMA-8 Theoretical Array Pattern over Frequency')
+
+plt.show()
