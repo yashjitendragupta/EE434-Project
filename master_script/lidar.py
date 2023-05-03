eddacd6d9a9b2ea8cedc53a88976fb75ed0c99a7
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2023-05-02 18:17:26 -0700

    Move interpolation to another thread * Effectively is a pipeline between interpolation and velocity detection * This maxes out GPU utilization and raises performance to about 8 fps

--- master_script/lidar.py
+++ master_script/lidar.py
@@ -9,7 +9,7 @@ import numpy as np
 from rplidar import RPLidar
 from queue import Queue
 from threading import Thread
-from time import time
+from time import time, sleep
 
 import motion
 
@@ -24,7 +24,7 @@ class RpLidarContext(RPLidar):
 # thetas_txt = open("thetas.txt","w")
 
 interp = motion.interpolator(memory_size=256, meters=True, radians=True)
-filt = motion.low_pass_filter(360, corner_freq=0.5, sampling_freq=4) # so far, the best sampling freq observed is about 4 Hz
+filt = motion.low_pass_filter(360, corner_freq=0.5, sampling_freq=8) # so far, the best sampling freq observed is about 8 Hz
 vel_est = motion.angular_velocity_estimator(window_size=16)
 
 with RpLidarContext('/dev/ttyUSB0', baudrate=115200) as lidar:
@@ -32,43 +32,65 @@ with RpLidarContext('/dev/ttyUSB0', baudrate=115200) as lidar:
 	
 	# the queuing rplidar uses seems flaky, so we'll run our own queue and monitior the queue's size
 	def lidar_routine():
+		global samples_queue
+
 		try:
 			samples_batch = []
 			for samples in lidar.iter_scans(max_buf_meas=512):
 				samples_batch.extend(samples)
-				if len(samples_batch) >= 256:
+				if len(samples_batch) >= 128:
 					samples_queue.put(samples_batch) # pass samples into samples_queue
 					samples_batch = []
 		except Exception as e: # if an error is encountered, pass that error to the main thread
 			samples_queue.put(e)
 
+	frame_rate = 0
+	shared_heights = np.zeros(360, dtype=np.float32) # shared between interpolation_thread and main thread
+	ready_flag = False
+
+	# since the interpolation routine primarily uses torch (which releases the GIL), we can use an asynchronous thread
+	def interpolation_routine():
+		global frame_rate, shared_heights, ready_flag, samples_queue
+		
+		prev_ts = time()
+		while True:
+			samples = samples_queue.get(timeout=3)
+			if isinstance(samples, Exception): # TODO: raise this exception in the main thread
+				raise samples
+			
+			samples = np.array(samples)
+			samples = samples[::2] # downsample by 2
+			angles = samples[:, 1]
+			distances = samples[:, 2]
+
+			interp.insert_many(angles, distances)
+			angle_grid, shared_heights = interp.generate()
+
+			ready_flag = True
+
+			current_ts = time()
+			frame_rate = 1/(current_ts-prev_ts)
+			prev_ts = current_ts
+
 	lidar_thread = Thread(target=lidar_routine, daemon=True)
 	lidar_thread.start()
+	interpolation_thread = Thread(target=interpolation_routine, daemon=True)
+	interpolation_thread.start()
 
 	prev_ts = time()
 	while True:
-		samples = samples_queue.get(timeout=1) # collect samples from samples_queue
-		if isinstance(samples, Exception): # if an error was received from the lidar daemon, raise it here
-			raise samples
-		
 		# collect dt as a difference between timestamps
 		current_ts = time()
 		dt = current_ts - prev_ts
 		prev_ts = current_ts
 
 		# Do processing to find the three theta values
-		
-		angles = []
-		distances = []
-		for sample in samples[::2]: # downsample by 2
-			angles.append(sample[1])
-			distances.append(sample[2])
-		angles = np.array(angles)
-		distances = np.array(distances)
 
-		interp.insert_many(angles, distances)
-		angle_grid, heights = interp.generate()
-		filtered_heights = filt.filter(heights)
+		while not ready_flag:
+			sleep(0.010)
+		ready_flag = False
+		
+		filtered_heights = filt.filter(shared_heights)
 		velocities = vel_est.estimate(filtered_heights, dt)
 		labels = motion.detect_motion(velocities)
 		indicator = motion.find_motion(labels)
@@ -80,8 +102,8 @@ with RpLidarContext('/dev/ttyUSB0', baudrate=115200) as lidar:
 		velocity_repr[velocity_repr > 9] = 9
 
 		# report the velocity representation, dt, and queue size (queue size shouldn't keep growing!)
-		print(velocity_repr, 'dt: %.2f' % dt, 'qsize: %02d' % samples_queue.qsize(), end='\r')
-
+		print(velocity_repr, 'dt: %.2f frame_rate: %01.1f qsize: %02d' % (dt, frame_rate, samples_queue.qsize()), end='\r')
+		
 		# Write theta values to text file, this is defo not the right way but whatevs we'll fix it l8r
 		# thetas_txt.truncate(0)
 		# thetas_txt.write("-1 -1 -1")
